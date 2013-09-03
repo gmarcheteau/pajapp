1acbcb015d5a32674f417fd29f1202cfa667efc3
gmarcheteau <gregoire.marcheteau@gmail.com>
2013-09-03 22:48:49 +0200

    first commit from Eclipse

---
+++ PajApp/src/com/bigotapps/pajapp/MainActivity.java
@@ -0,0 +1,215 @@
+package com.bigotapps.pajapp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements SensorEventListener {
+
+
+	/**
+	 * sensor stuff 
+	 */
+	private SensorManager mSensorManager;
+	private Sensor mSensor;
+	public int sensorSensitivity=6;
+	/**
+	 * Minimum delay between sensor readings
+	 * To avoid conflicts 
+	 */
+	public long lastSensorUpdate = 0;
+	public long delay =0;
+	public long minDelay = 10;
+	
+	/**
+	 * PajaMetrics
+	 */
+	public boolean RIGHT_HAND=true;
+	public boolean IS_DOWN;
+	public int distance = 0;
+	public int targetDistance = 50;
+	public int countSlow=1;
+	public int countFast=1;
+	public int countVeryFast=1;
+	
+	/**TIMING 
+	 */
+	public boolean PAJA_STARTED = false;
+	public long startTime;
+	public long duration;
+	public long lastChange;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public boolean onOptionsItemSelected(MenuItem item){
+		switch(item.getItemId()){
+		case R.id.change_hand:
+			//action
+			changeHands();
+			return true ;
+		default:
+			return false;
+		}
+	}
+
+
+	@Override
+	  protected void onResume() {
+	    super.onResume();
+	    mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+	  }
+
+	  @Override
+	  protected void onPause() {
+	    super.onPause();
+	    distance=0;
+	    mSensorManager.unregisterListener(this);
+	  }
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		if(!PAJA_STARTED){
+			startTime=System.currentTimeMillis();
+			PAJA_STARTED=true;
+		}
+		
+		// check last sensor update action
+		delay=System.currentTimeMillis()-lastSensorUpdate;
+		View back= findViewById(R.id.relLayout);
+		View hand = findViewById(R.id.imageView1);
+		TextView dist = (TextView) findViewById(R.id.textViewDistance);
+		TextView slow = (TextView) findViewById(R.id.TextViewSlow);
+		TextView fast = (TextView) findViewById(R.id.TextViewFast);
+		
+		//if Paja is completed
+		if(distance==targetDistance){
+			endPaja(); //should pass parameters (paja score, etc)
+		}
+		
+		else{
+		
+			if(delay>minDelay){
+				//UP
+				if(event.values[1]<sensorSensitivity){
+							//hand up
+					hand.scrollTo(0, 150);
+					lastSensorUpdate=System.currentTimeMillis();
+					IS_DOWN=false;
+						}
+				else{
+					//hand down
+					if(!IS_DOWN){
+						hand.scrollTo(0, -150);
+						distance++;
+						dist.setText(Integer.toString(distance));
+						IS_DOWN=true;
+						lastSensorUpdate=System.currentTimeMillis();
+						
+						//rythm
+						long timeSinceLastChange=System.currentTimeMillis()-lastChange;
+						
+						if(distance>3 && timeSinceLastChange<200){ //VERY FAST
+							fast.setVisibility(View.INVISIBLE);
+							slow.setVisibility(View.INVISIBLE);
+							back.setBackgroundColor(getResources().getColor(R.color.bigRed));
+							countVeryFast++;}
+						else if(distance>3 && timeSinceLastChange<300){ //FAST
+							fast.setVisibility(View.VISIBLE);
+							slow.setVisibility(View.INVISIBLE);
+							back.setBackgroundColor(getResources().getColor(R.color.bigWhite));
+							countFast++;}
+						else if(distance>3 && timeSinceLastChange>500){ //SLOW
+							slow.setVisibility(View.VISIBLE);
+							fast.setVisibility(View.INVISIBLE);
+							back.setBackgroundColor(getResources().getColor(R.color.bigBlue));
+							countSlow++;}
+						else {back.setBackgroundColor(getResources().getColor(R.color.bigWhite));}
+						
+						
+						lastChange=System.currentTimeMillis();
+						}
+				 	}
+				
+			}
+		}
+	}
+			   
+    public void changeHands(){
+    	View hand = findViewById(R.id.imageView1);
+    	ImageView iv = (ImageView) hand;
+    	if(RIGHT_HAND){
+    		iv.setImageResource(R.drawable.openhandleft);}
+    	else
+    		{iv.setImageResource(R.drawable.openhandright);}
+    	
+    	RIGHT_HAND=!RIGHT_HAND;
+    }
+    
+	public int getScore(){
+		int coeff_Slow=2;
+		int coeff_Fast=1;
+		int coeff_VeryFast=3;
+		int coeff_Duration=1;
+		return Math.round(
+				countSlow*coeff_Slow*
+				countFast*coeff_Fast*
+				countVeryFast*coeff_VeryFast*
+				duration*coeff_Duration
+				/100);
+	}
+    
+    public void endPaja(){
+		
+		mSensorManager.unregisterListener(this);
+		duration=(System.currentTimeMillis()-startTime)/1000;
+    	Intent i = new Intent(getApplicationContext(),PajaCompleted.class);
+		i.putExtra("score", String.valueOf(getScore()));
+		i.putExtra("duration", String.valueOf(duration));
+		startActivity(i);
+    }
+    
+    /**
+	 * QuickToaster
+	 * for runtime debugging etc 
+	 */
+	public void quickToast (String message){
+		Context context = getApplicationContext();
+		int duration = Toast.LENGTH_SHORT;
+		Toast toast = Toast.makeText(context, message, duration);
+		toast.show();	
+	}
+}
+	
+
---
+++ PajApp/src/com/bigotapps/pajapp/PajaCompleted.java
@@ -0,0 +1,71 @@
+package com.bigotapps.pajapp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class PajaCompleted extends Activity {
+
+	public long[] pattern = {10,1000,100,1000,100,1000};
+	String score;
+	String duration;
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.paja_completed);
+		
+		// Get instance of Vibrator from current Context
+		Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+		 
+		// Vibrate according to pattern (-1 means don't repeat)
+		v.vibrate(pattern,-1);
+
+		
+		playSound();
+		
+		if(this.getIntent().getExtras() != null){
+			//use paja parameters
+			score = this.getIntent().getExtras().getString("score");
+			duration = this.getIntent().getExtras().getString("duration");
+			TextView scoreView = (TextView) findViewById(R.id.textViewScore);
+			scoreView.setText(score+" pts");
+			quickToast("Paja score: "+ score +"\n Duration: " + duration + "s");
+		}
+		
+	}
+	
+	public void playSound(){
+		MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.open);
+		mp.start();
+	}
+	
+	public void sharePaja(View v){
+		Intent intent = new Intent(Intent.ACTION_SEND);
+		intent.setType("text/html");
+		intent.putExtra(Intent.EXTRA_SUBJECT, "Te env’an una paja");
+		intent.putExtra(Intent.EXTRA_TEXT, "Hola,\n \nUn amigo quiere compartir contigo la siguiente paja: \n \nDuraci—n: "+duration+"s \nScore: "+score+" pts");
+		startActivity(Intent.createChooser(intent, "Send Email"));
+	}
+	
+	 public void newPaja(View v){
+	    	Intent i = new Intent(getApplicationContext(),MainActivity.class);
+			startActivity(i);
+	    }
+	
+	 /* QuickToaster
+	 * for runtime debugging etc 
+	 */
+	public void quickToast (String message){
+		 Context context = getApplicationContext();
+			int duration = Toast.LENGTH_SHORT;
+			Toast toast = Toast.makeText(context, message, duration);
+			toast.show();
+	 }
+}
+
